56b00edd1bec2afa02528bcbcbfe82a55f6e89dc
Maverick7274 <neelanjanmukerji88@gmail.com>
2024-08-31 00:11:55 +0530

    Styling Maybe fixed

--- src/Components/Tutorials/Tutorial.tsx
+++ src/Components/Tutorials/Tutorial.tsx
@@ -16,27 +16,31 @@ export default function Tutorial(props: Props) {
 	return (
 		<div className={cn("min-h-screen flex justify-center", "relative")}>
 			<div className="absolute top-[-69px] left-[-400px] w-[1103px] h-[1017px] bg-[radial-gradient(35.19%_35.19%_at_50%_50%,rgba(59,149,255,0.17)_0%,rgba(28,106,197,0)_100%)] overflow-clip"></div>
-            <div className="absolute w-[513px] bg-[rgba(0,0,0,0.2)] h-full z-[-1] right-0 hidden md:flex">
-
-            </div>
-			<div className={cn("grid md:grid-cols-2 grid-cols-1 items-center justify-center gap-14")}>
-				<div className="flex flex-col justify-start gap-5">
-					<h1 className={cn("h1")}>
-						{titleWords.join(" ")}{" "}
-						<span className={cn("h1", "last-word")}>
-							{lastWord}
-						</span>
-					</h1>
-					<div className="">
-						<div className="w-20 h-[0.15rem] bg-primary"></div>
+			<div className="absolute w-[513px] bg-[rgba(0,0,0,0.2)] h-full z-[-1] right-0 hidden md:flex"></div>
+			<div className="flex items-center justify-center w-full">
+				<div
+					className={cn(
+						"grid md:grid-cols-2 grid-cols-1 content-center items-center justify-between gap-32 w-full px-0 md:px-32",
+					)}
+				>
+					<div className="flex flex-col justify-start gap-5">
+						<h1 className={cn("h1")}>
+							{titleWords.join(" ")}{" "}
+							<span className={cn("h1", "last-word")}>
+								{lastWord}
+							</span>
+						</h1>
+						<div className="">
+							<div className="w-20 h-[0.15rem] bg-primary"></div>
+						</div>
+						<div className="font-normal">{props.description}</div>
 					</div>
-					<div className="font-normal">{props.description}</div>
-				</div>
-				<div className="">
+
 					<Image
+						className="flex items-center justify-center"
 						src={props.image}
 						alt={props.title}
-						width={400}
+						width={450}
 						height={1079}
 					/>
 				</div>
