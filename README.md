e63df19246de2a4629713b7efa70284d5fb5b8c2
Maverick7274 <neelanjanmukerji88@gmail.com>
2024-09-01 16:34:42 +0530

    Description Component made dynamic

--- README.md
+++ README.md
@@ -1,8 +1,45 @@
-# CirclePe Assignment
+# [CirclePe](https://circlepe.com/) Assignment
+
+This is a simple project which lists all the steps on how CirclePe's App Interface works in just 5 Minutes.
+
+## Techincal Stack used to build the project
+
+[![Next.js](https://img.shields.io/badge/Next.js-000000?style=for-the-badge&logo=next.js&logoColor=white)](https://nextjs.org/)
+
+[![React](https://img.shields.io/badge/React-61DAFB?style=for-the-badge&logo=react&logoColor=white)](https://reactjs.org/)
+
+[![Tailwind CSS](https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white)](https://tailwindcss.com/)
+
+[![TypeScript](https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white)](https://www.typescriptlang.org/)
+
+[![Framer Motion](https://img.shields.io/badge/Framer_Motion-0055FF?style=for-the-badge&logo=framer&logoColor=white)](https://www.framer.com/motion/)
+
+[![Cloudinary](https://img.shields.io/badge/Cloudinary-4285F4?style=for-the-badge&logo=cloudinary&logoColor=white)](https://cloudinary.com/)
+
+[![Vercel](https://img.shields.io/badge/Vercel-000000?style=for-the-badge&logo=vercel&logoColor=white)](https://circlepe-assignment.vercel.app/)
+
+* This project is built using [Next.js](https://nextjs.org/) for it's Server Side Rendering,
+* For ease in styling I've used [Tailwind CSS](https://tailwindcss.com/),
+* For Type Safety I've used [TypeScript](https://www.typescriptlang.org/),
+* for Fluid Animations I've used [Framer Motion](https://www.framer.com/motion/),
+* To store the images I've used [Cloudinary](https://cloudinary.com/),
+* The project is hosted on [Vercel](https://vercel.com/).
 
 ## To run the project locally
 
-First, run the development server:
+First, run a command to install the dependencies:
+
+```bash
+npm install
+# or
+yarn install
+# or
+pnpm install
+# or
+bun install
+```
+
+Then, run the development server:
 
 ```bash
 npm run dev
@@ -16,21 +53,66 @@ bun dev
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+## The Approach
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+I've followed a simple approach where I try and make almost every component and setting reusable and scalable, which is the main point of using a Virtual DOM library like React.
 
-## Learn More
+I've used a simple folder structure where I've kept all the components with their names in the `Components` folder.
 
-To learn more about Next.js, take a look at the following resources:
+## The Process
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+First I wrote down all the data in a simple json object where we list all the steps required for [CirclePe](https://circlepe.com/)'s App interface to work.
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+This ensures that the data can be changed easily and the UI will be updated accordingly without us hardcoding the data in the components.
 
-## Deploy on Vercel
+## How the main data is structured and handles
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+For maximizing reusability this data was written to parse through the `props` system of React.
 
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+I've also added interface interactions like:
+
+* Boolean values to show and hide arrows according to the developer or one's wish.
+
+* Style settings for positioning the arrow(if the `arrowVisible` is set to `true`).
+
+The real reason I choose Next.js was because we can natively render `tsx` format in a field of a JSON object. Like in the `description` field of the JSON object.
+
+I made different components for different types of `description` like:
+
+* `OneLine` for a single line of text,
+* `TwoLines` for two lines of text,
+* `BulletPoints` for a heading bullet points,
+
+```TypeScript
+const steps = [
+     {
+  id: 1,
+  title: 'Step 1',
+  description: (
+   <TwoLines
+    active
+    text=""
+    textArray={[
+     'Tenant selects the property',
+     'Tenant selects flexible rent tenure & corresponding amount',
+    ]}
+    activeIndex={0}
+   />
+  ),
+  image: 'https://res.cloudinary.com/da1x0nwv3/image/upload/v1725031973/how-it-works_oaxb1q.png',
+  scaleImage: true,
+  scaleImageNumber: 1.5,
+  arrowVisible: true,
+  arrowImage:
+   'https://res.cloudinary.com/da1x0nwv3/image/upload/v1725062349/arrow_fcgub4.png',
+  arrowTop: 'top-[200px]',
+  arrowLeft: 'left-[-350px]',
+ },
+];
+```
+
+## How Media is handled
+
+I've used Cloudinary to store the images and then used the URL to display the images in the project.
+
+I've also used a dev dependency called [`svgr/webpack`](https://react-svgr.com/docs/next/) to convert the SVGs to React Components.
--- src/Components/Tutorials/TutorialsRecycler.tsx
+++ src/Components/Tutorials/TutorialsRecycler.tsx
@@ -1,209 +1,136 @@
-"use client";
+'use client';
 
-import Tutorial from "@/Components/Tutorials/Tutorial";
-import Bullet from "@/Assets/bulletpoint.png";
-import WhiteBullet from "@/Assets/white-bulletpoint.png";
-import Image from "next/image";
-import Slider from "@/Components/Slider";
-import HorizontalSlider from "@/Components/HorizontalSlider";
-import { cn } from "@/libs/utils";
-import { useEffect, useRef } from "react";
-import { useScroll, motion, useTransform } from "framer-motion";
+import Tutorial from '@/Components/Tutorials/Tutorial';
+import YellowBullet from '@/Assets/YellowBullet.svg';
+import WhiteBullet from '@/Assets/WhiteBullet.svg';
+import Image from 'next/image';
+import Slider from '@/Components/Slider';
+import HorizontalSlider from '@/Components/HorizontalSlider';
+import { cn } from '@/libs/utils';
+import { useEffect, useRef } from 'react';
+import { useScroll, motion, useTransform } from 'framer-motion';
+import { BulletPoints, SingleLine, TwoLines } from './Description';
 
 const steps = [
 	{
 		id: 0,
-		title: "How does it work?",
-		description:
-			"We make it possible in a quick and easy few steps process, takes max 5 mins.",
+		title: 'How does it work?',
+		description: (
+			<SingleLine
+				text="We make it possible in a quick and easy few steps process, takes max 5 mins."
+				active={false}
+			/>
+		),
 		arrowVisible: false,
-		image: "https://res.cloudinary.com/da1x0nwv3/image/upload/v1725031973/how-it-works_oaxb1q.png",
+		image: 'https://res.cloudinary.com/da1x0nwv3/image/upload/v1725031973/how-it-works_oaxb1q.png',
 	},
 	{
 		id: 1,
-		title: "Step 1",
+		title: 'Step 1',
 		description: (
-			<div>
-				<p className="flex items-center justify-start gap-5">
-					<span>
-						<Image
-							src={Bullet}
-							alt="bullet"
-							width={16}
-							height={16}
-							className="hidden md:flex"
-						/>
-					</span>
-					<span className="text-active font-medium">
-						Tenant selects the property
-					</span>
-				</p>
-				<p className="flex items-center justify-start gap-5">
-					<span>
-						<Image
-							src={WhiteBullet}
-							alt="bullet"
-							width={16}
-							height={16}
-							className="hidden md:flex"
-						/>
-					</span>
-					<span className="font-medium">
-						Tenant selects flexible rent tenure & corresponding
-						amount
-					</span>
-				</p>
-			</div>
+			<TwoLines
+				active
+				text=""
+				textArray={[
+					'Tenant selects the property',
+					'Tenant selects flexible rent tenure & corresponding amount',
+				]}
+				activeIndex={0}
+			/>
 		),
-		image: "https://res.cloudinary.com/da1x0nwv3/image/upload/v1725031973/how-it-works_oaxb1q.png",
+		image: 'https://res.cloudinary.com/da1x0nwv3/image/upload/v1725031973/how-it-works_oaxb1q.png',
 		scaleImage: true,
 		scaleImageNumber: 1.5,
 		arrowVisible: true,
 		arrowImage:
-			"https://res.cloudinary.com/da1x0nwv3/image/upload/v1725062349/arrow_fcgub4.png",
-		arrowTop: "top-[200px]",
-		arrowLeft: "left-[-350px]",
+			'https://res.cloudinary.com/da1x0nwv3/image/upload/v1725062349/arrow_fcgub4.png',
+		arrowTop: 'top-[200px]',
+		arrowLeft: 'left-[-350px]',
 	},
 	{
 		id: 2,
-		title: "Step 1.5",
+		title: 'Step 1.5',
 		description: (
-			<div>
-				<p className="flex items-center justify-start gap-5">
-					<span>
-						<Image
-							src={WhiteBullet}
-							alt="bullet"
-							width={16}
-							height={16}
-							className="hidden md:flex"
-						/>
-					</span>
-					<span className="font-medium">
-						Tenant selects the property
-					</span>
-				</p>
-				<p className="flex items-center justify-start gap-5">
-					<span>
-						<Image
-							src={Bullet}
-							alt="bullet"
-							width={16}
-							height={16}
-							className="hidden md:flex"
-						/>
-					</span>
-					<span className="text-active font-medium">
-						Tenant selects flexible rent tenure & corresponding
-						amount
-					</span>
-				</p>
-			</div>
+			<TwoLines
+				text=""
+				active
+				textArray={[
+					'Tenant selects the property',
+					'Tenant selects flexible rent tenure & corresponding amount',
+				]}
+				activeIndex={1}
+			/>
 		),
-		image: "https://res.cloudinary.com/da1x0nwv3/image/upload/v1725034702/step-1_fwslws.png",
+		image: 'https://res.cloudinary.com/da1x0nwv3/image/upload/v1725034702/step-1_fwslws.png',
 		scaleImage: true,
 		scaleImageNumber: 1.5,
 		arrowVisible: true,
 		arrowImage:
-			"https://res.cloudinary.com/da1x0nwv3/image/upload/v1725062349/arrow_fcgub4.png",
-		arrowTop: "top-[320px]",
-		arrowLeft: "left-[-250px]",
-		arrowRight: "",
-		arrowRotate: "rotate-[-180deg]",
+			'https://res.cloudinary.com/da1x0nwv3/image/upload/v1725062349/arrow_fcgub4.png',
+		arrowTop: 'top-[320px]',
+		arrowLeft: 'left-[-250px]',
+		arrowRight: '',
+		arrowRotate: 'rotate-[-180deg]',
 	},
 	{
 		id: 3,
-		title: "Step 2",
+		title: 'Step 2',
 		description: (
-			<div>
-				<p className="text-active flex items-center justify-start gap-5">
-					<span>
-						<Image
-							src={Bullet}
-							alt="bullet"
-							width={16}
-							height={16}
-							className="hidden md:flex"
-						/>
-					</span>
-					<span className="font-medium">
-						Tenant selects Pay with Circle enabling:
-					</span>
-				</p>
-				<ul className="list-disc list-inside leading-7 ml-7">
-					<li>Zero security deposit move-in</li>
-					<li>Reduced rent offer</li>
-					<li>3 months salary cover</li>
-				</ul>
-			</div>
+			<BulletPoints
+				active
+				text="Tenant gets approved to move-in"
+				bulletPoints={[
+					'Zero security deposit move-in',
+					'Reduceed rent offer',
+					'3 months salary cover',
+				]}
+			/>
 		),
-		image: "https://res.cloudinary.com/da1x0nwv3/image/upload/v1725034702/step-2_n4wyqj.png",
+		image: 'https://res.cloudinary.com/da1x0nwv3/image/upload/v1725034702/step-2_n4wyqj.png',
 		arrowVisible: true,
-		arrowTop: "top-[350px]",
-		arrowLeft: "left-[-250px]",
-		arrowRight: "",
+		arrowTop: 'top-[350px]',
+		arrowLeft: 'left-[-250px]',
+		arrowRight: '',
 		arrowImage:
-			"https://res.cloudinary.com/da1x0nwv3/image/upload/v1725062349/arrow_fcgub4.png",
+			'https://res.cloudinary.com/da1x0nwv3/image/upload/v1725062349/arrow_fcgub4.png',
 	},
 	{
 		id: 4,
-		title: "Step 3",
+		title: 'Step 3',
 		description: (
-			<p className="text-active flex items-center justify-start gap-5">
-				<span>
-					<Image
-						src={Bullet}
-						alt="bullet"
-						width={16}
-						height={16}
-						className="hidden md:flex"
-					/>
-				</span>
-				<span className="font-medium">
-					Smooth Onboarding for the Tenant begins
-				</span>
-			</p>
+			<SingleLine
+				text="Smooth Onboarding for the Tenant begins"
+				active={true}
+			/>
 		),
-		image: "https://res.cloudinary.com/da1x0nwv3/image/upload/v1725031973/step-3_cgbzyn.png",
+		image: 'https://res.cloudinary.com/da1x0nwv3/image/upload/v1725031973/step-3_cgbzyn.png',
 		arrowVisible: true,
-		arrowTop: "top-[420px]",
-		arrowLeft: "left-[-130px]",
-		arrowRight: "",
+		arrowTop: 'top-[420px]',
+		arrowLeft: 'left-[-130px]',
+		arrowRight: '',
 		arrowImage:
-			"https://res.cloudinary.com/da1x0nwv3/image/upload/v1725062349/arrow_fcgub4.png",
+			'https://res.cloudinary.com/da1x0nwv3/image/upload/v1725062349/arrow_fcgub4.png',
 	},
 	{
 		id: 5,
-		title: "Step 4",
+		title: 'Step 4',
 		description: (
-			<div>
-				<p className="text-active flex items-center justify-start gap-5">
-					<span>
-						<Image
-							src={Bullet}
-							alt="bullet"
-							width={16}
-							height={16}
-							className="hidden md:flex"
-						/>
-					</span>
-					<span className="font-medium">
-						Tenant gets approved to move-in:
-					</span>
-				</p>
-				<ul className="list-disc list-inside leading-7 ml-7">
-					<li>Get Zero-security deposit approval</li>
-					<li>Zero cost EMI = Monthly Rent</li>
-				</ul>
-			</div>
+			<BulletPoints
+				active
+				text="Tenant gets approved to move-in"
+				bulletPoints={[
+					'Gets Zero-security deposit approval',
+					'Zero cost EMI = Monthly Rent',
+				]}
+			/>
 		),
-		image: "https://res.cloudinary.com/da1x0nwv3/image/upload/v1725031973/step-4_aeul90.png",
+		image: 'https://res.cloudinary.com/da1x0nwv3/image/upload/v1725185601/step-4_pvvkfe.png',
 		arrowVisible: true,
-		arrowTop: "top-[230px]",
-		arrowLeft: "left-[-220px]",
-		arrowRight: "",
+		arrowTop: 'top-[230px]',
+		arrowLeft: 'left-[-220px]',
+		arrowRight: '',
 		arrowImage:
-			"https://res.cloudinary.com/da1x0nwv3/image/upload/v1725062349/arrow_fcgub4.png",
+			'https://res.cloudinary.com/da1x0nwv3/image/upload/v1725062349/arrow_fcgub4.png',
 	},
 ];
 
@@ -212,7 +139,7 @@ export default function Home() {
 
 	const { scrollYProgress } = useScroll({
 		target: container,
-		offset: ["end end", "start start"],
+		offset: ['end end', 'start start'],
 	});
 
 	const translateY = useTransform(scrollYProgress, [0, 1], [500, -100]);
@@ -228,17 +155,18 @@ export default function Home() {
 			transition: {
 				duration: 1,
 				delay: 1.5,
-				type: "spring",
+				type: 'spring',
 			},
 		},
 	};
 
 	useEffect(() => {
 		async () => {
-			const LocomotiveScroll = (await import("locomotive-scroll")).default;
+			const LocomotiveScroll = (await import('locomotive-scroll'))
+				.default;
 			const locomotiveScroll = new LocomotiveScroll();
 			locomotiveScroll.init();
-		}
+		};
 	}, []);
 
 	return (
@@ -251,13 +179,13 @@ export default function Home() {
 					style={{ y: translateY }}
 					transition={{
 						repeat: Infinity,
-						repeatType: "mirror",
-						ease: "easeInOut",
+						repeatType: 'mirror',
+						ease: 'easeInOut',
 						duration: 0.5,
 					}}
 					className={cn(
-						"top-[-190px] left-[-400px] w-[1103px] h-[1017px]",
-						"hidden md:flex absolute bg-[radial-gradient(35.19%_35.19%_at_50%_50%,rgba(59,149,255,0.17)_0%,rgba(28,106,197,0)_100%)] overflow-clip"
+						'top-[-190px] left-[-400px] w-[1103px] h-[1017px]',
+						'hidden md:flex absolute bg-[radial-gradient(35.19%_35.19%_at_50%_50%,rgba(59,149,255,0.17)_0%,rgba(28,106,197,0)_100%)] overflow-clip',
 					)}
 				></motion.div>
 			</motion.div>
@@ -287,4 +215,4 @@ export default function Home() {
 			</div>
 		</main>
 	);
-};
+}
